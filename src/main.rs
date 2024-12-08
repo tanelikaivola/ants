13c03e4711659dc7e2f30aae16c4551a71f6de36
Toni <perala.toni@gmail.com>
2024-12-08 22:14:00 +0200

    Take interface name as an argument

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,34 @@ mod tarpitter;
 mod tcp_listener;
 
 use std::env;
+use std::process;
 
-fn parse_arguments() -> bool {
+fn parse_arguments() -> (bool, String) {
     let args: Vec<String> = env::args().collect();
-    args.contains(&"--passive".to_string())
+
+    let passive_mode = args.contains(&"--passive".to_string()) || args.contains(&"-p".to_string());
+
+    let interface_index = args.iter().position(|arg| arg == "-i");
+    let interface_name = match interface_index {
+        Some(index) => {
+            if let Some(name) = args.get(index + 1) {
+                name.clone()
+            } else {
+                eprintln!("Error: No value provided for '-i' flag.");
+                process::exit(1);
+            }
+        }
+        None => {
+            eprintln!("Error: The '-i <interface_name>' flag is mandatory.");
+            process::exit(1);
+        }
+    };
+
+    (passive_mode, interface_name)
 }
 
 fn main() {
-    let passive_mode = parse_arguments();
+    let (passive_mode, interface_name) = parse_arguments();
 
-    tarpitter::start_tarpitting(passive_mode);
+    tarpitter::start_tarpitting(passive_mode, &interface_name);
 }
--- src/tarpitter/mod.rs
+++ src/tarpitter/mod.rs
@@ -2,13 +2,11 @@ use crate::{arp_listener, tcp_listener};
 
 use std::sync::mpsc;
 
-pub fn start_tarpitting(passive_mode: bool) {
-    const INTERFACE_NAME: &str = "wlo1";
-
-    let rx = arp_listener::start_arp_handling(INTERFACE_NAME, passive_mode);
+pub fn start_tarpitting(passive_mode: bool, interface_name: &str) {
+    let rx = arp_listener::start_arp_handling(interface_name, passive_mode);
 
     let (ip_sender, ip_receiver) = mpsc::channel();
-    tcp_listener::start_tcp_tarpitting(INTERFACE_NAME, ip_receiver, passive_mode);
+    tcp_listener::start_tcp_tarpitting(interface_name, ip_receiver, passive_mode);
 
     for target_ip in rx {
         println!("Tarpitting IP: {}", target_ip);
